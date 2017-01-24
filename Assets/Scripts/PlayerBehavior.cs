772d3569bd0db8b68d90c1b9de620b6d45f20235
Nicolas Naugle <naugle92@ufl.edu>
2017-01-23 20:08:40 -0500

    player moves in all four directions by use of the arrow keys

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ReadInput ();
+	}
+
+	void ReadInput() {
+		float x = this.transform.position.x;
+		float y = this.transform.position.y;
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			x -= speed;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			x += speed;
+		} else if (Input.GetKey (KeyCode.UpArrow)) {
+			y += speed;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			y -= speed;
+		}
+		this.transform.position = new Vector3 (x, y, 0.0f);
+	}
+}
